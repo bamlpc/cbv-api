771a0cd6cb0870770f15c978728a31720904856c
bmogetta <brunomogetta@gmail.com>
2023-01-15 19:02:46 -0300

    ci: fix keystack array bug

--- src/helpers/graphql_post_handler.ts
+++ src/helpers/graphql_post_handler.ts
@@ -5,11 +5,12 @@ import { KeyStack } from 'deps';
 
 
 async function store_cbv_with_credentials(args: Record<string, CBV>): Promise<string | Error> {
-	const keyStack = new KeyStack(API_KEY[0]);
-	//TODO : CHECK KEY VALIDATION FAILING
+	const keyStack = new KeyStack([API_KEY[0]]);
 	const foreign_api_key = args.cbv.api_key!;
 
-	if (!await keyStack.verify(API_KEY[1], foreign_api_key)) {
+	const authorized = await keyStack.verify(API_KEY[1], foreign_api_key)
+
+	if (!authorized) {
 		return Error('Unauthorized');
 	}
 
