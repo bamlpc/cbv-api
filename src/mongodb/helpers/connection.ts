291b2e2d478d9b0285445ad05221eb7d9fd936c9
Bruno Mogetta <100445144+BMogetta@users.noreply.github.com>
2023-09-18 18:48:10 -0300

    Update connection.ts

--- src/mongodb/helpers/connection.ts
+++ src/mongodb/helpers/connection.ts
@@ -2,25 +2,37 @@ import { mongo } from 'deps';
 import { MONGODB } from 'environment';
 import { StoredMongoCBVSchema } from 'schemas';
 
-// iniciate mongo
 const client = new mongo.MongoClient();
 
-await client.connect({
-	db: MONGODB.DB_NAME,
-	tls: true,
-	servers: [
-		{
-			host: MONGODB.URL, //"cluster0-shard-00-01.adchc.mongodb.net"
-			port: 27017,
-		},
-	],
-	credential: {
-		username: MONGODB.ID,
-		password: MONGODB.PASS,
-		db: MONGODB.DB_NAME,
-		mechanism: 'SCRAM-SHA-1',
-	},
-});
+const hosts = MONGODB.URL.split(',');
+
+let isConnected = false;
+for (const host of hosts) {
+    try {
+        await client.connect({
+            db: MONGODB.DB_NAME,
+            tls: true,
+            servers: [{
+                host: host.trim(),
+                port: 27017,
+            }],
+            credential: {
+                username: MONGODB.ID,
+                password: MONGODB.PASS,
+                db: MONGODB.DB_NAME,
+                mechanism: 'SCRAM-SHA-1',
+            },
+        });
+        isConnected = true;
+        break;  // break the loop if connection is successful
+    } catch (err) {
+        console.error(`Failed to connect to ${host}. Trying the next one...`);
+    }
+}
+
+if (!isConnected) {
+    throw new Error('Failed to connect to all MongoDB hosts.');
+}
 
 // Access Collection
 const db = client.database(MONGODB.DB_NAME);
